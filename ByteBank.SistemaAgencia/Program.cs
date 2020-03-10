4c98b8560c688f29e3ed6acbba32d136ac9dc5d8
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2020-03-10 13:49:12 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AutenticacaoHelper teste;//É visivel somente dentro da biblioteca (projeto), quando ultiliza o modificador de acesso internal
+
+            ContaCorrente conta = new ContaCorrente(1612,58974);
+
+            
+            Console.WriteLine(conta.Numero);
+            Console.ReadLine();
+        }
+    }
+}
